bc6ddf49a4e92e5aae83ab738bccadf0dc538621
samuelgebing <samu.gebing@gmail.com>
2026-04-10 20:39:26 -0300

    Fix indentation in server.js for consistency

--- module-2/theory/server.js
+++ module-2/theory/server.js
@@ -21,5 +21,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
- console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
